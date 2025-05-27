2f6edb56cbb3e2cc99822d846e5e0a3b7c467fac
Bobby <gibob130@gmail.com>
2025-05-27 11:19:40 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e  # Hentikan script jika terjadi error
+
 # Nama asli dan nama baru
 SOURCE_URL="https://raw.githubusercontent.com/gibobb/check_service/main/check_service"
 FINAL_NAME="check_service"
@@ -8,7 +10,7 @@ PATH_BIN2="/usr/local/bin/check_service"
 
 # Download dari GitHub
 echo "Downloading script from GitHub..."
-curl -L "$FINAL_NAME" "$SOURCE_URL"
+curl -Lo "$FINAL_NAME" "$SOURCE_URL"
 
 # Cek apakah file berhasil di-download
 if [ ! -f "$FINAL_NAME" ]; then
@@ -16,9 +18,13 @@ if [ ! -f "$FINAL_NAME" ]; then
     exit 1
 fi
 
-# Ubah nama file
-sudo install -m 755 "$FINAL_NAME" /usr/bin/check_service
+# Pindahkan file dan beri izin eksekusi
+echo "Installing script to /usr/bin and creating symlink in /usr/local/bin..."
+sudo install -m 755 "$FINAL_NAME" "$PATH_BIN"
 sudo ln -sf "$PATH_BIN" "$PATH_BIN2"
+
+# Bersihkan file sementara
+rm -f "$FINAL_NAME"
 
 # Konfirmasi
 echo "Done. You can now run the script by typing: check_service"
